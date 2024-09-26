010362f5110ac7586a05765466d4d20df8584efa
Bamboozill <george.chase.ab@gmail.com>
2024-09-26 15:17:58 -0600

    Ch2 lesson 6

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill(color="black")
+        player.update(dt)
         player.draw(screen)
         pygame.display.flip()
         clock.tick(60)
